1b7af1dfebe98d9c9ed74adb06d47fd3bc8d4b9d
Phùng Anh Minh <dieglevel@gmail.com>
2025-03-30 14:26:58 +0700

    Refactor application to use dotenv for environment variables, update file upload handling, and enhance UI with new styling

--- index.js
+++ index.js
@@ -1,98 +1,145 @@
 const express = require('express');
 const app = express();
+const path = require('path')
+const { v4: uuid } = require("uuid")
+require('dotenv').config()
 
+app.use(express.json({extended: false}));  
 app.use(express.static('./views'))
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-// Dynamo
 const AWS = require('aws-sdk');
-const config = new AWS.Config(configAWS)
-
+const config = new AWS.Config({
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    region: 'us-east-1'
+});
 AWS.config = config;
+const s3 = new AWS.S3()
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+
 
 const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName = "SanPham";
 
-const tableName = 'dynamoDB';
 
-// Multer
 const multer = require('multer');
-const { configAWS } = require('./env');
 
-const upload = multer();
+const storage = multer.memoryStorage({
+    destination(req, file, callback){
+        callback(null, '');
+    },
+});
+
+function checkFileType(file, cb){
+    const fileTypes = /jpeg|jpg|png|gif/;
+
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase())
+    const minetype = fileTypes.test(file.mimetype)
 
 
-app.get('/', (req, res) => {
-
-    const params = {
-        TableName: tableName,
+    if (extname && minetype){
+        return cb(null,true)
     }
 
+    return cb("Error: Image Only")
+}   
+
+const upload = multer({
+    storage,
+    limits: { fileSize:2000000 }, 
+    fileFilter(req, file, cb){
+        console.log(file)
+        checkFileType(file, cb);
+    }
+});
+
+app.get("/", (request, response) => {
+    const params = { TableName: tableName };
+
     docClient.scan(params, (err, data) => {
-        if (err){
-            res.send("Internal Server Error")
+        if (err) {
+            response.send("Internal Server Error");
         } else {
-            return res.render('index', {dynamoDB: data.Items});
+            return response.render("index", { sanPhams: data.Items });
+        }
+    });
+});
+
+const CLOUD_FRONT_URL = 'https://d3gwzzvwt1bn1m.cloudfront.net'
+
+app.post("/", upload.single('image'), (req, res) => {
+    const { MaSP, TenSP, SoLuong } = req.body;
+    const image = req.file.originalname.split(".");
+
+    const fileType = image[image.length-1];
+
+    const filePath = `${uuid() + Date.now().toString()}.${fileType}`
+    const params = {
+        Bucket: "lab7-bukets3",
+        Key: filePath,
+        Body: req.file.buffer
+    }
+
+    s3.upload(params, (error, data) =>{
+        if(error) {
+            console.log('error = ', error)
+            return res.send('Internal Server Error')
+        }else{
+            const newItem = {
+                TableName: tableName,
+                Item: {
+                    "MaSP": MaSP,
+                    "TenSP": TenSP,
+                    "SoLuong": SoLuong,
+                    "url": `${CLOUD_FRONT_URL}/${filePath}`
+                }
+            };
+        
+        
+            docClient.put(newItem, (err, data) => {
+                if (err) {
+                    return res.send("Internal Server Error");
+                } else {
+                    return res.redirect("/");
+                }
+            });
         }
     })
 });
 
-
-app.post('/', upload.fields([]), (req, res) => {
-    const {ma_sp, ten_sp, so_luong} = req.body;
-
-    const params = {
-        TableName: tableName,
-        Item: {
-            "ma_sp": ma_sp,
-            "ten_sp": ten_sp,
-            "so_luong": so_luong
-        }
-    }
-    
-    docClient.put(params, (err, data) => {
-        if (err){
-            console.log(err)
-            return res.send("Internal Server Error")
-        } else {
-           return res.redirect("/")
-        }
-    })
-
-})
-
 app.post("/delete", upload.fields([]), (req, res) => {
-    const listItems = Object.keys(req.body);
+    const listItems = Object.values(req.body);
 
     if (listItems.length === 0) {
         return res.redirect("/");
     }
 
+
     function onDeleteItem(index) {
         const params = {
             TableName: tableName,
             Key: {
-                "ma_sp": listItems[index]
+                "MaSP": listItems[index]+""
             }
-        }
+        };
 
         docClient.delete(params, (err, data) => {
             if (err) {
-                return res.send("Internal Server Error")
-            }
-            else {
+                return res.send("Internal Server Error");
+            } else {
                 if (index > 0) {
-                    onDeleteItem(index -1);
+                    onDeleteItem(index - 1);
                 } else {
-                    return res.redirect("/")
+                    return res.redirect("/");
                 }
             }
-        })
+        });
     }
 
-    onDeleteItem(listItems.length -1)
-})
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    onDeleteItem(listItems.length - 1);
 });
